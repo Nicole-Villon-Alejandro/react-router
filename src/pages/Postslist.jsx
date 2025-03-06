86ee9f1274e5ed6f5988618eb48a2f8c5470023b
Nicole-Villon-Alejandro <nicolevillon5@gmail.com>
2025-03-06 23:12:00 +0100

    add: changes

--- src/pages/Postslist.jsx
+++ src/pages/Postslist.jsx
@@ -2,8 +2,55 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 const Postslist = () => {
+
+    const [posts, setPosts] = useState([]);
+
+    const url = import.meta.env.VITE_ENDPOINT_URL
+
+    useEffect( () => {
+        axios.get (url)
+        .then ( (res) => setPosts(res.data))
+        .catch( (err) => console.error (err))
+    } )
+
+
     return (
+        <>
         <h1>Posts List</h1>
+
+        <div className="container">
+            <div className="row">
+                {
+                    posts.map ( (elem) => {
+
+                        const { id, title, content, image, tags } = elem;
+
+
+                        return(
+                            <div className="col-3" key={id}>
+                                <div className="card">
+                                    <div className="card-body">
+                                        <h4 className="card-title">
+                                            {elem.title}
+
+                                        </h4>
+                                        <p className="card-text"> {elem.tags}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            
+
+
+                        )
+
+                    } )
+
+                }
+            </div>
+        </div>
+
+        </>
+        
     )
 }
 
